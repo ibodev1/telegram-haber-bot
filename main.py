b3bc9cb82bb7d29b5364312696a8f4ce75578c71
İbrahim Ödev <74017534+ibodev1@users.noreply.github.com>
2021-11-04 21:26:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,30 +8,35 @@ print("Bot çalışıyor...")
 
 
 def start_command(update, context):
-    update.message.reply_text('Merhaba Hoşgeldin. \n/sonhaber \n/tumu')
+    update.message.reply_text(f'Merhaba @{update.message.chat.username} ,\n \nProfil Bilgilerin : \nAd Soyad : {update.message.chat.first_name} {update.message.chat.last_name} \nİD : {update.message.chat.id} \n \n Yardım Kodları : \n/sonhaber : Son Haberi Çeker \n/tumu : Tüm Haberleri Çeker')
 
 def son_haber(update, context):
+    print(f"Kullanıcı Son Haberi Çekti : {update.message.chat.username} ")
     r = requests.get("https://www.cumhuriyet.com.tr/gundem")
-    s = BeautifulSoup(r.content,"lxml")
+    s = BeautifulSoup(r.content,"html.parser")
     haber = s.find("div",attrs={"class":"haber"})
     haber_foto = haber.find("div",attrs={"class":"haber-foto"}).find("img")
     haber_baslik = haber.find("div",attrs={"class":"haber-baslik"}).find("a")
-    update.message.reply_text(haber_baslik.get("title") + "\n \n Haber Link : " + "https://www.cumhuriyet.com.tr"+ str(haber_baslik.get("href")) + "\n \n Resim Link : " + "https://www.cumhuriyet.com.tr"+ str(haber_foto.get("src")))
+    update.message.reply_photo("https://www.cumhuriyet.com.tr"+ str(haber_foto.get("src")))
+    update.message.reply_markdown("**" + haber_baslik.get("title") + "** \n \n \n [Haber Linki (Gitmek İçin Tıkla)](" + "https://www.cumhuriyet.com.tr"+ str(haber_baslik.get("href")) +")", disable_web_page_preview=True)
     pass
 
 def tumu(update, context):
+    print(f"Kullanıcı Tüm Haberleri Çekti : {update.message.chat.username} ")
     r = requests.get("https://www.cumhuriyet.com.tr/gundem")
-    s = BeautifulSoup(r.content,"lxml")
+    s = BeautifulSoup(r.content,"html.parser")
     haber = s.find_all("div",attrs={"class":"haber"})
     for news in haber:
         news_image = news.find("div",attrs={"class":"haber-foto"}).find("img")
         news_title = news.find("div",attrs={"class":"haber-baslik"}).find("a")
-        update.message.reply_text(news_title.get("title") + "\n \n Haber Link : " + "https://www.cumhuriyet.com.tr"+ str(news_title.get("href")) + "\n \n Resim Link : " + "https://www.cumhuriyet.com.tr"+ str(news_image.get("src")))
+        update.message.reply_photo("https://www.cumhuriyet.com.tr"+ str(news_image.get("src")))
+        update.message.reply_markdown("**" + news_title.get("title") + "** \n \n \n [Haber Linki (Gitmek İçin Tıkla)](" + "https://www.cumhuriyet.com.tr"+ str(news_title.get("href")) +")", disable_web_page_preview=True)
     update.message.reply_text("Haber Sayısı : " + str(len(haber)))
     pass
 
 def help_command(update, context):
-    update.message.reply_text('/sonhaber : Son Haberi Çeker \n/tumu : Tüm Haberleri Çeker')
+    print(f"Kullanıcı Yardım İstedi : {update.message.chat.username} ")
+    update.message.reply_text(f'Merhaba @{update.message.chat.username} ,\n \nProfil Bilgilerin : \nAd Soyad : {update.message.chat.first_name} {update.message.chat.last_name} \nİD : {update.message.chat.id} \n \nYardım Kodları : \n/sonhaber : Son Haberi Çeker \n/tumu : Tüm Haberleri Çeker')
 
 def handle_message(update, context):
     text = str(update.message.text).lower()
@@ -41,7 +46,9 @@ def handle_message(update, context):
 
 
 def error(update, context):
-    print(f"Update : {update} Error : {context.error}")
+    print("\n \n>>>>> H A T A <<<<<\n \n")
+    print(f"> Gönderen Bilgisi : \n> {update} \n \n> Hata Mesajı :  \n> {context.error}")
+    print("\n \n>>>>><<<<<>>>>><<<<<\n \n")
 
 def main():
     updater = Updater(keys.API_KEY, use_context=True)
--- responses.py
+++ responses.py
@@ -3,7 +3,10 @@ from datetime import datetime
 def sample_responses(input_text):
     user_message = str(input_text).lower()
 
-    if user_message in ("deneme", "sa"):
-        return "As"
+    if user_message in ("sa", "Sa", "Sea"):
+        return "Aleyküm selam"
+
+    if user_message in ("merhaba", "Merhaba"):
+        return "Merhaba"
 
     return "Yanlış veya bilinmeyen giriş yaptınız."
